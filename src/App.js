93f75b1f8395fb1c54bd1b71f708bd12f0d6d4db
Diogo\Snor <df.duarte@campus.fct.unl.pt>
2017-11-05 17:33:46 +0000

    Created Register component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import axios from 'axios';
 
-class App extends Component {
+
+class Register extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      first_name:'',
+      last_name:'',
+      email:'',
+      password:''
+    }
+  }
+
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Register"
+           />
+           <TextField
+             hintText="Enter your First Name"
+             floatingLabelText="First Name"
+             onChange = {(event,newValue) => this.setState({first_name:newValue})}
+             />
+           <br/>
+           <TextField
+             hintText="Enter your Last Name"
+             floatingLabelText="Last Name"
+             onChange = {(event,newValue) => this.setState({last_name:newValue})}
+             />
+           <br/>
+           <TextField
+             hintText="Enter your Email"
+             type="email"
+             floatingLabelText="Email"
+             onChange = {(event,newValue) => this.setState({email:newValue})}
+             />
+           <br/>
+           <TextField
+             type = "password"
+             hintText="Enter your Password"
+             floatingLabelText="Password"
+             onChange = {(event,newValue) => this.setState({password:newValue})}
+             />
+           <br/>
+           <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+          </div>
+         </MuiThemeProvider>
       </div>
     );
   }
-}
 
-export default App;
+  handleClick(event){
+
+    console.log("values",this.state.first_name,this.state.last_name,this.state.email,this.state.password);
+    //To be done:check for empty values before hitting submit
+    var self = this;
+    var payload={
+    "first_name": this.state.first_name,
+    "last_name":this.state.last_name,
+    "email":this.state.email,
+    "password":this.state.password
+    }
+    
+  }
+  }
+
+
+const style = {
+  margin: 15,
+};
+
+export default Register;
