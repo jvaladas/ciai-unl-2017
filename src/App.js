b2510b0238d54b0187c2e38c5e7e51c26f825cc4
Diogo\Snor <df.duarte@campus.fct.unl.pt>
2017-11-10 18:06:29 +0000

    Art upload 1.0

--- src/App.js
+++ src/App.js
@@ -49,7 +49,8 @@ class App extends Component {
             "FirstName":"Andre",
             "LastName":"Maria",
             "Email": "andre@aol.com.br",
-            "Password": "123"
+            "Password": "123",
+            "Art":[]
           }
         ]
     }
@@ -330,6 +331,9 @@ class Account extends React.Component{
         <li>
           <DashboardListItem></DashboardListItem>
         </li>
+        <li>
+          <ImageUpload/>
+        </li>
       </ul>
             
         </div>
@@ -339,6 +343,61 @@ class Account extends React.Component{
   }
 }
 
+
+class ImageUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {file: '',imagePreviewUrl: ''};
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+    console.log('handle uploading-', this.state.file);
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  render() {
+    let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<img src={imagePreviewUrl} />);
+    } else {
+      $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
+    }
+
+    return (
+      <div className="previewComponent">
+        <form onSubmit={(e)=>this._handleSubmit(e)}>
+          <input className="fileInput" 
+            type="file" 
+            onChange={(e)=>this._handleImageChange(e)} />
+          <button className="submitButton" 
+            type="submit" 
+            onClick={(e)=>this._handleSubmit(e)}>Upload Image</button>
+        </form>
+        <div className="imgPreview">
+          {$imagePreview}
+        </div>
+      </div>
+    )
+  }
+}
 
 
 const style = {
